d9525c8abe706713361304cefc4f7d03f90026d7
Chanty-ctrl <ch8nty@hotmail.co.uk>
2022-05-11 08:51:04 +0100

    NHS Digital API Web App
    LIVE system of REST API test

---
+++ NHS_Digit_LookUp/Scripts/system-scripts/todo.js
@@ -0,0 +1,4 @@
+
+//https://digital.nhs.uk/services/organisation-data-service/file-downloads/gp-and-gp-practice-related-data?msclkid=45ebdbf9c19411ec938ed684fb4cf581
+
+//https://directory.spineservices.nhs.uk/ORD/2-0-0/organisations?PrimaryRoleId=RO177
